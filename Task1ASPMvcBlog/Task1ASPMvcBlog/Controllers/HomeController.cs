fe23db0b5ddd8ae2989a5ebfe5e07a062a5f7f93
Hohovem <Hohovem@gmail.com>
2020-02-03 16:12:59 +0200

    first commit

---
+++ Task1ASPMvcBlog/Task1ASPMvcBlog/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task1ASPMvcBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Reviews()
+        {
+            ViewBag.Message = "Your application reviews page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Profile()
+        {
+            ViewBag.Message = "Your profile page";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Profile(FormCollection form)
+        {
+            ViewBag.TextReturn = "Вы ввели" + form["dishName"];
+
+            return View();
+        }
+    }
+}
