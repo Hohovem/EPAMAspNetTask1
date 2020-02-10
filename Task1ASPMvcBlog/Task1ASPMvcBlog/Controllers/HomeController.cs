cbd66b15ebae16e285d56ce5d5815290826a4804
Hohovem <Hohovem@gmail.com>
2020-02-10 07:50:02 +0200

    middle

--- Task1ASPMvcBlog/Task1ASPMvcBlog/Controllers/HomeController.cs
+++ Task1ASPMvcBlog/Task1ASPMvcBlog/Controllers/HomeController.cs
@@ -3,21 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Task1ASPMvcBlog.Models;
 
 namespace Task1ASPMvcBlog.Controllers
 {
     public class HomeController : Controller
     {
+        ICollection<CommentModel> Comments = new List<CommentModel>
+        {
+            new CommentModel(),
+            new CommentModel(),
+            new CommentModel(),
+            new CommentModel(),
+            new CommentModel()
+        };
+
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Reviews(string commentName, string content)
+        {
+            Comments.Add(new CommentModel(
+                imageArdress: "~/Content/Images/Review/defaultCommentImage.jpg",
+                senderName:commentName, 
+                content:content, 
+                postDate:"11.2.15"));
+
+            return View(Comments);
+        }
+
+        [HttpGet]
         public ActionResult Reviews()
         {
             ViewBag.Message = "Your application reviews page.";
 
-            return View();
+            
+
+            return View(Comments);
         }
 
         [HttpGet]
---
+++ Task1ASPMvcBlog/Task1ASPMvcBlog/Models/CommentModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Task1ASPMvcBlog.Models
+{
+    public class CommentModel
+    {
+        public CommentModel()
+        {
+            ImageArdress = "~/Content/Images/Review/defaultCommentImage.jpg";
+            SenderName = "DefaultName";
+            Content = "Bla bla blah";
+            PostDate = "05/29/2015";
+        }
+
+        public CommentModel(string imageArdress, string senderName, string content, string postDate)
+        {
+            ImageArdress = imageArdress;
+            SenderName = senderName;
+            Content = content;
+            PostDate = postDate;
+        }
+
+        public string ImageArdress { get; }
+        public string SenderName { get; }
+        public string Content { get; }
+        public string PostDate { get; }
+    }
+}
